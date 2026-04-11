f833708f6f202c65756d7475e9542c43ee8ff9bd
Natasha Card <natasha.card@outlook.com>
2026-04-11 19:46:49 +0100

    Restore original index.tsx

--- index.tsx
+++ index.tsx
@@ -1,1 +1,10 @@
-import 'expo-router/entry';
+import { registerRootComponent } from 'expo';
+import * as AppModule from './App';
+
+const App = AppModule.default;
+
+if (!App) {
+  console.error('App import is undefined');
+}
+
+registerRootComponent(App);
